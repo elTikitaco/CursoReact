818e02d9bb2deb74ecdf58228ba13da167f84e7f
Jose Ramon <L20170804@culiacan.tecnm.mx>
2026-02-18 12:48:14 -0700

    Paso 52. pasar argumentos personalizados a funciones de evento

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./assets/components/Header";
 import CoreConcept from "./assets/components/CoreConcept";
 import {CORE_CONCEPTS} from "./data.js"
+import TabButton from "./assets/components/TabButton";
+
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components','jsx','props','state'
+        console.log(`Selected: ${selectedButton}`);
+    }
   return (
     <div>
       <Header/>
@@ -18,6 +24,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section>
+          <h2>Examples</h2>
+          <menu>
+                <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
